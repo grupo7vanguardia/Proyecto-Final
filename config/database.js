189b5f03fb916304b74b0b7cf54565c8b406582a
Wilfredo Paiz <wilfredopaizpaz@gmail.com>
2018-11-23 09:20:47 -0600

    Cambio DB

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-    //database: 'mongodb://localhost:27017/examen_ej2',
-    database: 'mongodb://localhost:27017/proyectovanguardia',
+    //database: 'mongodb://localhost:27017/proyectovanguardia',
+    
+    database: 'mongodb://wilpaiz:wilpaiz10@ds151141.mlab.com:51141/proyectovanguardia2',
     secret: 'mysupersecret'  
 }
