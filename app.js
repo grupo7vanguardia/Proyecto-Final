8342e135a35b5aa7387027edc38e520d59fe5cf0
Wilfredo Paiz <wilfredopaizpaz@gmail.com>
2018-12-11 11:43:25 -0600

    FRONTEND MOUNTED

--- app.js
+++ app.js
@@ -26,9 +26,14 @@ mongoose.connection.on('error', (err) => {
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/Angular-Front/dist/client'));
+
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/Angular-Front/dist/client/index.html'));
+});
 
 app.use(logger('dev'));
 app.use(express.json());
